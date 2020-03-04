0f7e3007261b008f6c452754ddcef5b626760d86
tranhuyssj100 <tranhuyssj100@gmail.com>
2020-03-04 18:42:29 +0700

    no message

--- Assets/WordChef/Common/Scripts/Dictiony/Dictionary.cs
+++ Assets/WordChef/Common/Scripts/Dictiony/Dictionary.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Net;
-using System;
 using UnityEngine;
+using Newtonsoft.Json;
+
+
 
 public class Dictionary : MonoBehaviour
 {
+   
     public void GetData()
     {
-        //Debug.Log("hdasdasd");
+        Debug.Log("hdasdasd");
         var client = new WebClient();
-        var text = client.DownloadString("https://slack.com/intl/en-vn/");
-        Debug.Log(text.ToString());
+        var text = client.DownloadString("https://api.tracau.vn/WBBcwnwQpV89/s/hello/en");
+        //var json= JsonConvert.SerializeObject(text);
+        //Debug.Log(json);
+        
+    }
+
+    public class DataDictionary
+    {
+        public string languague;
+        public List<Sentence> senteneces;
+        
+    } 
+    
+    public class Sentence
+    {
+        List<Field> fields;
+    }
+    
+    public class Field
+    {
+        string en;
+        string vi;
     }
 }
