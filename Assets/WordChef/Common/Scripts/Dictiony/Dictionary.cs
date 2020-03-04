5469b0ae835d41ee46dd5e7bf9fcc579291f82d4
tranhuyssj100 <tranhuyssj100@gmail.com>
2020-03-04 17:19:21 +0700

    no message

---
+++ Assets/WordChef/Common/Scripts/Dictiony/Dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System;
+using UnityEngine;
+
+public class Dictionary : MonoBehaviour
+{
+    public void GetData()
+    {
+        //Debug.Log("hdasdasd");
+        var client = new WebClient();
+        var text = client.DownloadString("https://slack.com/intl/en-vn/");
+        Debug.Log(text.ToString());
+    }
+}
