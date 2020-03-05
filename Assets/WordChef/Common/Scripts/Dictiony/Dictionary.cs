cbee02993d9a0326d6ec53b0b520663982cb17e0
tranhuyssj100 <tranhuyssj100@gmail.com>
2020-03-05 18:57:46 +0700

    demo Dictionary

--- Assets/WordChef/Common/Scripts/Dictiony/Dictionary.cs
+++ Assets/WordChef/Common/Scripts/Dictiony/Dictionary.cs
@@ -3,37 +3,74 @@ using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine.UI;
 
 
 
 public class Dictionary : MonoBehaviour
 {
-   
-    public void GetData()
+    public string wordValid;
+    public bool includeRelated;
+    public string sourceDictionaries;
+    public bool useCanonical;
+    public bool includeTags;
+    public string limit;
+    public Text textWordName;
+    public Text textMean;
+
+    string keyApi;
+    string url;
+    Dictionary<string, List<WordData>> wordsDict= new Dictionary<string, List<WordData>>();
+    List<WordData> listMeanWord = new List<WordData>();
+    WordData wordData;
+
+    private void Start()
     {
-        Debug.Log("hdasdasd");
-        var client = new WebClient();
-        var text = client.DownloadString("https://api.tracau.vn/WBBcwnwQpV89/s/hello/en");
-        //var json= JsonConvert.SerializeObject(text);
-        //Debug.Log(json);
-        
+        sourceDictionaries = "wiktionary";
+        keyApi = "l7bgsd9titsbw82vtpzparyfrmt9yg2hibbeihv5uex8e5maa";
+        url = "https://api.wordnik.com/v4/word.json/" + wordValid
+            + "/definitions?limit=" + limit
+            + "&includeRelated=" + includeRelated
+            + "&sourceDictionaries=" + sourceDictionaries
+            + "&useCanonical=" + useCanonical
+            + "&includeTags=" + includeTags
+            + "&api_key=" + keyApi;
     }
 
-    public class DataDictionary
+    public void GetData()
     {
-        public string languague;
-        public List<Sentence> senteneces;
-        
-    } 
-    
-    public class Sentence
-    {
-        List<Field> fields;
+        char[] s = { '[', ']' };
+        var client = new WebClient();
+        var text = client.DownloadString(url);
+        JArray arrayJson = JArray.Parse(text);
+        for(int i=0; i < arrayJson.Count; i++)
+        {
+            wordData = JsonConvert.DeserializeObject<WordData>(arrayJson[i].ToString());
+            listMeanWord.Add(wordData);
+        }
+
+        wordsDict.Add(wordValid,listMeanWord);
     }
-    
-    public class Field
+
+    public void ShowWordData()
     {
-        string en;
-        string vi;
+        GetData();
+          foreach (KeyValuePair<string, List<WordData>> word in wordsDict)
+        {
+            textWordName.text = word.Key;
+            foreach(WordData wordMean in  word.Value)
+            {
+                textMean.text += wordMean.text + ", ";
+            }
+        }
     }
 }
+
+public class WordData 
+{
+    public string partOfSpeech;
+    public string text;
+    
+}
+
