afa8b151041ac3516254a8d24bcc94a07e3e38cf
dattq <datquoc9x@gmail.com>
2020-04-20 12:22:16 +0700

    - fix price item, reward ads, level clear

--- Assets/WordChef/_Scripts/Const.cs
+++ Assets/WordChef/_Scripts/Const.cs
@@ -6,11 +6,11 @@ public class Const
     public const float CELL_GAP_COEF_X = 0.01f;
     public const float CELL_GAP_COEF_Y = 0.16f;
     public const float COL_GAP_COEF = 0.6f;
-    public const int HINT_COST = 120;
-    public const int HINT_RANDOM_COST = 360;
-    public const int REWARD_CHAPTER_CLEAR = 30;
-    public const int REWARD_ADS_LEVEL_CLEAR = 40;
-    public const int REWARD_ADS_CHAPTER_CLEAR = 70;
+    public const int HINT_COST = 100;
+    public const int HINT_RANDOM_COST = 300;
+    public const int REWARD_CHAPTER_CLEAR = 25;
+    public const int REWARD_ADS_LEVEL_CLEAR = 20;
+    public const int REWARD_ADS_CHAPTER_CLEAR = 50;
     public const int SPIN_AGAIN = 100;
 
     public const int NUM_SUBWORLD = 5;
--- Assets/WordChef/_Scripts/Main/RewardedVideoDialog.cs
+++ Assets/WordChef/_Scripts/Main/RewardedVideoDialog.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RewardedVideoDialog : Dialog {
+public class RewardedVideoDialog : Dialog
+{
     public Text amountText;
     public Text messageText;
 
     private void Start()
     {
-        SetAmount(40);
+        SetAmount(20);
     }
 
     public void SetAmount(int amount)
@@ -27,12 +28,25 @@ public class RewardedVideoDialog : Dialog {
     public void OnConfirmClick()
     {
         Sound.instance.Play(Sound.Others.PopupOpen);
-        MonoUtils.instance.ShowEffect(int.Parse(amountText.text.Replace("x ","")));
+        StartCoroutine(ShowEffectCollect(int.Parse(amountText.text.Replace("x ", ""))/5));
     }
 
-    public override void Close ()
-	{
-		base.Close ();
-		CurrencyController.CreditBalance(int.Parse(amountText.text.Replace("x ", "")));
-	}
+    public override void Close()
+    {
+        base.Close();
+        CurrencyController.CreditBalance(int.Parse(amountText.text.Replace("x ", "")));
+    }
+
+    private IEnumerator ShowEffectCollect(int value)
+    {
+        for (int i = 0; i < value; i++)
+        {
+            if (i < 5)
+            {
+                MonoUtils.instance.ShowEffect(value);
+            }
+            yield return new WaitForSeconds(0.02f);
+        }
+
+    }
 }
