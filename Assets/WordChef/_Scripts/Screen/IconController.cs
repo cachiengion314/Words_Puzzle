cb018973e1215e8216dabca57a5cbbb22d00bc20
dattqd96 <debug.logok@gmail.com>
2020-05-12 12:15:55 +0700

    - fix shadow scale man home

--- Assets/WordChef/_Scripts/Screen/IconController.cs
+++ Assets/WordChef/_Scripts/Screen/IconController.cs
@@ -19,22 +19,26 @@ public class IconController : MonoBehaviour
 
     private IEnumerator ShowIcon()
     {
+        var tweenControl = TweenControl.GetInstance();
         yield return new WaitForSeconds(_timeDelay);
         for (int i = 0; i < _buttons.Count; i++)
         {
             var btn = _buttons[i];
             var shawdow = _shadows[i];
-            TweenControl.GetInstance().ScaleFromZero(shawdow, 0.3f);
-            TweenControl.GetInstance().ScaleFromZero(btn, 0.3f, null, EaseType.InOutBack);
-            TweenControl.GetInstance().MoveRectY(btn.transform as RectTransform, -55, _timeMove, () =>
+            tweenControl.DelayCall(shawdow.transform, 0.1f, () =>
             {
-                TweenControl.GetInstance().MoveRectY(btn.transform as RectTransform, -100, 0.2f, () =>
+                tweenControl.ScaleFromZero(shawdow, 0.3f);
+            });
+            tweenControl.ScaleFromZero(btn, 0.3f, null, EaseType.InOutBack);
+            tweenControl.MoveRectY(btn.transform as RectTransform, -55, _timeMove, () =>
+            {
+                tweenControl.MoveRectY(btn.transform as RectTransform, -100, 0.2f, () =>
                 {
-                    TweenControl.GetInstance().MoveRectY(btn.transform as RectTransform, -73, 0.2f, () =>
+                    tweenControl.MoveRectY(btn.transform as RectTransform, -73, 0.2f, () =>
                     {
-                        TweenControl.GetInstance().MoveRectY(btn.transform as RectTransform, -87, 0.15f, () =>
+                        tweenControl.MoveRectY(btn.transform as RectTransform, -87, 0.15f, () =>
                         {
-                            TweenControl.GetInstance().MoveRectY(btn.transform as RectTransform, -85f, 0.15f, null, EaseType.Linear);
+                            tweenControl.MoveRectY(btn.transform as RectTransform, -85f, 0.15f, null, EaseType.Linear);
                         });
                     });
                 });
