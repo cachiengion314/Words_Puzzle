5c9368ff85a769aef4bb9ce14ae2edf6595b0836
Hoang Nam <cachiengion314@gmail.com>
2020-06-02 18:05:13 +0700

    Them tinh nang push Notification tren firebase

---
+++ Assets/WordChef/Common/Scripts/CloundMessagesFireBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Messaging;
+
+public class CloundMessagesFireBase : MonoBehaviour
+{
+    public void Start()
+    {
+        FirebaseMessaging.TokenReceived += OnTokenReceived;
+        FirebaseMessaging.MessageReceived += OnMessageReceived;
+    }
+    public void OnTokenReceived(object sender, TokenReceivedEventArgs token)
+    {
+        Debug.Log("Received Registration Token: " + token.Token);
+    }
+
+    public void OnMessageReceived(object sender, MessageReceivedEventArgs e)
+    {
+        Debug.Log("Received a new message from: " + e.Message.From);
+    }
+}
