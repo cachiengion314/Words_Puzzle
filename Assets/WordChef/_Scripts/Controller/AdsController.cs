5378f7d342671500f4a0f3d173bf2b2ddd086a6d
dattqd96 <debug.logok@gmail.com>
2020-06-09 16:23:08 +0700

    - adapter Ads

--- Assets/WordChef/_Scripts/Controller/AdsController.cs
+++ Assets/WordChef/_Scripts/Controller/AdsController.cs
@@ -4,41 +4,6 @@ using UnityEngine;
 
 public class AdsController : MonoBehaviour, IAds
 {
-    public void HandleAdsClosed()
-    {
-
-    }
-
-    public void HandleAdsFailedToLoad()
-    {
-
-    }
-
-    public void HandleAdsLeftApplication()
-    {
-
-    }
-
-    public void HandleAdsLoaded()
-    {
-
-    }
-
-    public void HandleAdsOpened()
-    {
-
-    }
-
-    public void HandleAdsRewarded()
-    {
-
-    }
-
-    public void HandleAdsStarted()
-    {
-
-    }
-
     public void ShowBannerAds()
     {
 
