b0e9772594f4c797b39249145d4d24d2af9a0cbc
dattqd96 <debug.logok@gmail.com>
2020-07-04 10:58:26 +0700

    - add sdk Android Performance

---
+++ Assets/WordPuzzle/Common/Scripts/GooglePerformance.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Google.Android.PerformanceTuner;
+using UnityEngine.Scripting;
+using Google.Protobuf.Reflection;
+
+public class GooglePerformance : MonoBehaviour
+{
+    AndroidPerformanceTuner<FidelityParams, Annotation> tuner =
+            new AndroidPerformanceTuner<FidelityParams, Annotation>();
+
+    private void Awake()
+    {
+        tuner.EnableLocalEndpoint();
+    }
+
+    void Start()
+    {
+        tuner.SetCurrentAnnotation(new Annotation
+        {
+            Scene = Scene.AssetswordpuzzleScenesmain,
+            LoadingState = LoadingState.Loading,
+            Difficulty = Difficulty.Medium
+            //GameDifficulty = Difficulty.Medium
+        });
+        tuner.SetFidelityParameters(new FidelityParams
+        {
+            ParticleDetails = 1.0f,
+            ShadowQualtity = ShadowQualtity.High,
+            TextureResolution = 2048,
+        });
+        ErrorCode startErrorCode = tuner.Start();
+
+        tuner.onReceiveUploadLog += request =>
+        {
+            tuner.SetCurrentAnnotation(new Annotation
+            {
+                Scene = Scene.AssetswordpuzzleScenesmain,
+                LoadingState = LoadingState.NotLoading,
+                Difficulty = Difficulty.Medium
+            });
+        };
+    }
+
+    // Don't call this method.
+    [Preserve]
+    void ExampleOfForceReflectionInitializationForProtobuf()
+    {
+        FileDescriptor.ForceReflectionInitialization<LoadingState>();
+        FileDescriptor.ForceReflectionInitialization<Scene>();
+        FileDescriptor.ForceReflectionInitialization<ShadowQualtity>();
+        FileDescriptor.ForceReflectionInitialization<Difficulty>();
+        // Add FileDescriptor.ForceReflectionInitialization<T> for each generated enum.
+        // You can find the list of enums in DevTuningfork.cs -> enum section
+        // or in the list of enums in Google -> Android Performance Tuner.
+    }
+}
