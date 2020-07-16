4f859446f5e0bfbebc0e6fd1709a7c67200cfd91
dattqd96 <debug.logok@gmail.com>
2020-07-16 10:11:04 +0700

    - add event log share dialog

--- Assets/WordPuzzle/Common/Scripts/Dialog/ShareDialog.cs
+++ Assets/WordPuzzle/Common/Scripts/Dialog/ShareDialog.cs
@@ -17,24 +17,33 @@ public class ShareDialog : Dialog
     {
         Sound.instance.PlayButton();
         NativeShareInvoker.instance.TakeScreenShotAndShareDelay(messengerPackageName);
+        DialogCallEventFirebase("share_messenger");
         Close();
     }
     public void OnFacebookClick()
     {
         Sound.instance.PlayButton();
         NativeShareInvoker.instance.TakeScreenShotAndShareDelay(facebookPackageName);
+        DialogCallEventFirebase("share_facebook");
         Close();
     }
     public void OnGmailClick()
     {
         Sound.instance.PlayButton();
         NativeShareInvoker.instance.TakeScreenShotAndShareDelay(gmailPackageName);
+        DialogCallEventFirebase("share_mail");
         Close();
     }
     public void OnAllAppClick()
     {
         Sound.instance.PlayButton();
         NativeShareInvoker.instance.TakeScreenShotAndShareDelay();
+        DialogCallEventFirebase("share_others");
         Close();
+    }
+
+    private void DialogCallEventFirebase(string nameEvent)
+    {
+        Firebase.Analytics.FirebaseAnalytics.LogEvent("share_dialog", nameEvent, nameEvent);
     }
 }
