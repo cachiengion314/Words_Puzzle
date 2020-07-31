1ac7d826138d43cb54babe50eba0541983f53b39
Hoang Nam <cachiengion314@gmail.com>
2020-07-31 09:31:08 +0700

    fix khoang cach word trong feedback (2)

--- Assets/WordPuzzle/Common/Scripts/Dialog/LevelWordFeedbackDialog.cs
+++ Assets/WordPuzzle/Common/Scripts/Dialog/LevelWordFeedbackDialog.cs
@@ -12,8 +12,8 @@ public class LevelWordFeedbackDialog : Dialog
     public static List<Toggle> toggleList = new List<Toggle>();
 
     private List<string> correctWordsDoneByPlayerList = new List<string>();
-    private List<RectTransform> textTransformPosList = new List<RectTransform>();
-    public RectTransform textBackgroundPrefab;
+ 
+    public RectTransform content;
     public RectTransform wordDoneByPlayerPrefab;
     public int currlevel;
     [Header("THEME UI CHANGE")]
@@ -37,20 +37,17 @@ public class LevelWordFeedbackDialog : Dialog
         currlevel = (GameState.currentLevel + numlevels * GameState.currentSubWorld + MainController.instance.gameData.words[0].subWords.Count * numlevels * GameState.currentWorld) + 1;
 
         WordsCorrectDoneByPlayer();
-        foreach (RectTransform child in textBackgroundPrefab)
-        {
-            textTransformPosList.Add(child);
-        }
+      
         if (correctWordsDoneByPlayerList.Count > 0)
         {
             for (int i = 0; i < correctWordsDoneByPlayerList.Count; i++)
             {
                 Toggle textToggle = Instantiate(wordDoneByPlayerPrefab).GetComponent<Toggle>();
                 textToggle.isOn = false;
-                textToggle.SetParent(textBackgroundPrefab);
+                textToggle.SetParent(content);
                 TextMeshProUGUI text = textToggle.GetComponentInChildren<TextMeshProUGUI>();
                 textToggle.transform.localScale = Vector3.one;
-                textToggle.transform.position = textTransformPosList[i].position;
+             
                 text.text = correctWordsDoneByPlayerList[i].ToString();
             }
         }
@@ -58,7 +55,7 @@ public class LevelWordFeedbackDialog : Dialog
 
     private void CheckTheme()
     {
-        if(MainController.instance != null)
+        if (MainController.instance != null)
         {
             var currTheme = ThemesControl.instance.CurrTheme;
             _btnSend.sprite = currTheme.uiData.levelWordData.btnSend;
@@ -90,7 +87,7 @@ public class LevelWordFeedbackDialog : Dialog
                 longText += textMeshPro.text.ToString();
         }
         if (longText == null || longText.Length == 0) { Close(); return; }
-      
+
         string key = MissingWordsFeedback._dataWordsRef.Push().Key;
         Dictionary<string, object> infoDic = new Dictionary<string, object>
         {
@@ -101,7 +98,7 @@ public class LevelWordFeedbackDialog : Dialog
             ["level"] = currlevel
         };
         // Push information
-        MissingWordsFeedback.childUpdates["/" + key] = infoDic;      
+        MissingWordsFeedback.childUpdates["/" + key] = infoDic;
         MissingWordsFeedback._dataWordsRef.UpdateChildrenAsync(MissingWordsFeedback.childUpdates);
 
         Close();
