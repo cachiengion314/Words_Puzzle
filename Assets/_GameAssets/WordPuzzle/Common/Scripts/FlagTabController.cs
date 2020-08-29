f73214b0365a48e0f8eb38d8779d3b544768a1fe
Hoang_Nam <namh@percas.vn>
2020-08-29 10:20:28 +0700

    clean code

--- Assets/_GameAssets/WordPuzzle/Common/Scripts/FlagTabController.cs
+++ Assets/_GameAssets/WordPuzzle/Common/Scripts/FlagTabController.cs
@@ -1,14 +1,6 @@
-﻿using DG.Tweening.Plugins.Core.PathCore;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using PlayFab.Internal;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 [Serializable]
@@ -40,12 +32,6 @@ public class FlagTabController : MonoBehaviour
     public HashSet<string> flagItemWordHashset = new HashSet<string>();
     public HashSet<string> unlockedWordHashset = new HashSet<string>();
 
-    private readonly string COUNTRY_NAME = "name";
-    private readonly string SUB_REGION = "subregion";
-    private readonly string CAPITAL = "capital";
-    private readonly string AREA = "area";
-    private readonly string POPULATION = "population";
-
     private bool isLoaded;
     private void Awake()
     {
@@ -71,80 +57,10 @@ public class FlagTabController : MonoBehaviour
     }
     public void CheckAndSaveCountrykWord(string wordIsChecking)
     {
-        LogController.Debug("wordIsChecking: " + wordIsChecking);
         string checkWord = wordIsChecking.ToLower();
         if (wordIsChecking == null || wordIsChecking == string.Empty || !flagItemWordHashset.Contains(checkWord)) return;
-
-        //string flagName = FindCountryNameWithUnlockedWord(wordIsChecking);
-        //Toast.instance.ShowMessage(flagName.ToUpper() + " Country found");
         AddToUnlockedWordDictionary(checkWord);
         SaveUnlockedWordData();
-    }
-    public void GetAllWordsList()
-    {
-        string allWordsString = null;
-        for (int i = 0; i < gameData.words.Count; i++)
-        {
-            for (int ii = 0; ii < gameData.words[i].subWords.Count; ii++)
-            {
-                for (int iii = 0; iii < gameData.words[i].subWords[ii].gameLevels.Count; iii++)
-                {
-                    allWordsString += "|" + gameData.words[i].subWords[ii].gameLevels[iii].answers;
-                }
-            }
-        }
-        allWordsList.Clear();
-        List<string> tempAllWordsList;
-        tempAllWordsList = allWordsString.Split(new string[1] { "|" }, StringSplitOptions.RemoveEmptyEntries).ToList();
-        tempAllWordsList.Sort();
-        tempAllWordsList = tempAllWordsList.Distinct().ToList();
-
-        allWordsList.AddRange(tempAllWordsList);
-    }
-    [HideInInspector] public bool isGetCountryRequestDone;
-    [HideInInspector] public bool haveInternet;
-
-    public IEnumerator GetCountryInfo(string countryName, string countryPopulation = "0")
-    {
-        isGetCountryRequestDone = false;
-        using (UnityWebRequest request = UnityWebRequest.Get("https://restcountries.eu/rest/v2/name/" + countryName))
-        {
-            yield return request.SendWebRequest();
-            while (!request.isDone)
-                yield return null;
-            isGetCountryRequestDone = true;
-
-            if (haveInternet)
-            {
-                byte[] result = request.downloadHandler.data;
-                string countryJSON = System.Text.Encoding.Default.GetString(result);
-                JArray jsonArr = JArray.Parse(countryJSON);
-                Dictionary<string, object> tempDic = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonArr[0].ToString());
-                countryInfo.Clear();
-                countryInfo = tempDic;
-                if (countryName.Equals("United Kingdom", StringComparison.OrdinalIgnoreCase))
-                {
-                    countryInfo[COUNTRY_NAME] = "United Kingdom";
-                }
-                countryInfo[POPULATION] = countryPopulation;
-            }
-
-        }
-    }
-    public IEnumerator GetCoutryInfoOffline(string coutryName)
-    {
-        FlagItem flagItem = flagItemList.Find((item) => item.flagName.Equals(coutryName, StringComparison.OrdinalIgnoreCase));
-        if (flagItem != null)
-        {
-            countryInfo.Clear();
-            countryInfo[COUNTRY_NAME] = flagItem.flagName;
-            LogController.Debug(flagItem.flagName);
-            countryInfo[SUB_REGION] = flagItem.subRegion;
-            countryInfo[CAPITAL] = flagItem.capital;
-            countryInfo[AREA] = flagItem.area;
-            countryInfo[POPULATION] = flagItem.population;
-        }
-        yield return null;
     }
     public void AddToUnlockedWordDictionary(string wordIsChecking)
     {
@@ -172,18 +88,5 @@ public class FlagTabController : MonoBehaviour
             flagItemWordHashset.Add(item.flagUnlockWord.ToLower());
         }
         isLoaded = true;
-    }
-
-    private string FindCountryNameWithUnlockedWord(string unlockedWord)
-    {
-        FlagItem flagItem = flagItemList.Find(flag => flag.flagUnlockWord.Equals(unlockedWord, StringComparison.OrdinalIgnoreCase));
-        if (flagItem != null)
-        {
-            return flagItem.flagName;
-        }
-        else
-        {
-            return "nation";
-        }
     }
 }
